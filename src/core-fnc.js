15bc17ebbeb65b8128ffd024b39fb12f5f54a316
Gediminas <gediminas.luzys@gmail.com>
2021-01-10 09:50:23 +0200

    ESLint

--- src/core-fnc.js
+++ src/core-fnc.js
@@ -1,6 +1,6 @@
-const { execFile } = require('child_process')
-const kill = require('tree-kill')
-const assert = require('better-assert')
+const { execFile } = require('child_process');
+const kill = require('tree-kill');
+const assert = require('better-assert');
 
 const processFullLines = (origBuffer, fnDoOnFullLine) => {
   const lines = origBuffer.split(/\r?\n/);
@@ -10,70 +10,66 @@ const processFullLines = (origBuffer, fnDoOnFullLine) => {
   return newBuffer;
 };
 
-const startTask = (task) => {
-  return new Promise((resolve, reject) => {
-    const args    = [task.product.script_path]
-    const options = { cwd: task.working_dir }
+const startTask = task => new Promise((resolve, reject) => {
+  const args    = [task.product.script_path];
+  const options = { cwd: task.working_dir };
 
-    const child = execFile(task.product.interpreter, args, options)
-    child.bufOut = ''
-    child.bufErr = ''
-    task.pid = child.pid
+  const child = execFile(task.product.interpreter, args, options);
+  child.bufOut = '';
+  child.bufErr = '';
+  task.pid = child.pid;
 
-    child.stdout.on('data', (data) => {
-      child.bufOut += data;
-      child.bufOut = processFullLines(child.bufOut, (text) => {
-        console.log(text);
-      });
+  child.stdout.on('data', (data) => {
+    child.bufOut += data;
+    child.bufOut = processFullLines(child.bufOut, (text) => {
+      console.log(text);
     });
+  });
 
-    child.stderr.on('data', (data) => {
-      child.bufErr += data;
-      child.bufErr = processFullLines(child.bufErr, (text) => {
-        console.error(text);
-      });
+  child.stderr.on('data', (data) => {
+    child.bufErr += data;
+    child.bufErr = processFullLines(child.bufErr, (text) => {
+      console.error(text);
     });
+  });
 
-    child.on('error', (error) => {
-      reject(error)
-    })
+  child.on('error', (error) => {
+    reject(error);
+  });
 
-    child.on('close', (exitCode) => {
-      assert(child.bufOut === '') // TODO send \n
-      assert(child.bufErr === '') // TODO send \n
-      delete task.pid
+  child.on('close', (exitCode) => {
+    assert(child.bufOut === ''); // TODO send \n
+    assert(child.bufErr === ''); // TODO send \n
+    delete task.pid;
 
-      switch (exitCode) {
-      case 0:  task.result = 'OK'; break
-      case 1:  task.result = 'WARNING'; break
-      case 2:  task.result = 'ERROR'; break
-      default: task.result = 'N/A'; break
-      }
-      resolve()
-    })
-  })
-}
+    switch (exitCode) {
+    case 0:  task.result = 'OK'; break;
+    case 1:  task.result = 'WARNING'; break;
+    case 2:  task.result = 'ERROR'; break;
+    default: task.result = 'N/A'; break;
+    }
+    resolve();
+  });
+});
 
-const killTask = (task) => {
-  return new Promise((resolve, reject) => {
-    kill(task.pid, 'SIGTERM', (error) => { // SIGKILL
-      if (error) {
-        reject(error)
-      } else {
-        resolve()
-      }
-    })
-  })
-}
+const killTask = task => new Promise((resolve, reject) => {
+  kill(task.pid, 'SIGTERM', (error) => { // SIGKILL
+    if (error) {
+      reject(error);
+    } else {
+      resolve();
+    }
+  });
+});
 
 const CanRun = (task, activeTasks) => {
-  const found_active = activeTasks.find(_task => _task.product_id === task.product_id)
-  return found_active ? true : false
-}
+  const found_active = activeTasks.find(_task => _task.product_id === task.product_id);
+  return found_active;
+};
 
 
 module.exports = {
   startTask,
   killTask,
   CanRun,
-}
+};

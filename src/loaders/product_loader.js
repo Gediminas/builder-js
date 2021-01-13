744e4dd82b026764d15a867f6d168becd2780843
Gediminas <gediminas.luzys@gmail.com>
2021-01-13 12:45:59 +0200

    Added sh interpreter

--- src/loaders/product_loader.js
+++ src/loaders/product_loader.js
@@ -1,42 +1,52 @@
-const fs = require('fs')
-const glob = require('glob')
-const path = require('path')
-const merge = require('merge')
-const configLoader = require('./config_loader.js')
+const fs = require('fs');
+const glob = require('glob');
+const path = require('path');
+const merge = require('merge');
+const configLoader = require('./config_loader.js');
 
-const cfgDef = configLoader.data.script_defaults
+const cfgDef = configLoader.data.script_defaults;
 
 const load_cfg = (script_dir, product_id) => {
-  const cfgPath = path.normalize(script_dir + product_id + '/script.cfg')
-  const srvPath = path.normalize(script_dir + product_id + '/server.cfg')
-  const cfg = JSON.parse(fs.readFileSync(cfgPath, 'utf8'))
-  const srv = JSON.parse(fs.readFileSync(srvPath, 'utf8'))
-  const mrg = merge.recursive(true, cfgDef, cfg, srv)
+  const cfgPath = path.normalize(script_dir + product_id + '/script.cfg');
+  const srvPath = path.normalize(script_dir + product_id + '/server.cfg');
+  const cfg = JSON.parse(fs.readFileSync(cfgPath, 'utf8'));
+  const srv = JSON.parse(fs.readFileSync(srvPath, 'utf8'));
+  const mrg = merge.recursive(true, cfgDef, cfg, srv);
   if (!mrg.product_name) {
-    mrg.product_name = product_id
+    mrg.product_name = product_id;
   }
-  return mrg
-}
+  return mrg;
+};
 
 const loadProducts = (script_dir, on_loaded) => {
   glob('*/index.*', { cwd: script_dir, matchBase: 1 }, (err, files) => {
     if (err) {
-      return
+      return;
     }
     const products = files.map((file) => {
-      const product_id = path.dirname(file)
-      const cfg = load_cfg(script_dir, product_id)
-      const script_js   = script_dir + file
+      const ext = path.extname(file);
+      const product_id = path.dirname(file);
+      const cfg = load_cfg(script_dir, product_id);
+      const script_js = script_dir + file;
+
+      let interpreter = '';
+      switch (ext) {
+      case '.js': interpreter = 'node'; break;
+      case '.sh': interpreter = 'sh'; break;
+      default:
+        return;
+      };
+
       return {
         product_id,
         product_name: cfg.product_name,
         cfg,
-        interpreter : 'node',
+        interpreter,
         script_path : script_js,
-      }
-    })
-    on_loaded(products)
-  })
-}
+      };
+    });
+    on_loaded(products);
+  });
+};
 
-module.exports = loadProducts
+module.exports = loadProducts;
